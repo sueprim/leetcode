99545dd6cd7a54f55f75cf999c2ece254a4a12a1
sueprim <164147163+sueprim@users.noreply.github.com>
2024-05-30 08:54:42 +0900

    Create 0101 Symmetric Tree.py

---
+++ 0101 Symmetric Tree.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        if not root: 
+            return True
+        return self.check(root.left, root.right) 
+        
+    def check(self, left, right):
+        if left is None and right is None: 
+            return True
+        if left is None or right is None:  
+            return False
+        if left.val != right.val:  
+            return False
+        a = self.check(left.left, right.right) 
+        b = self.check(left.right, right.left) 
+        return a and b 
