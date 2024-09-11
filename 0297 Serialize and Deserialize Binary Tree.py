dcfeee1b0f5707920af818615cd0f19d38e7a2ca
sueprim <164147163+sueprim@users.noreply.github.com>
2024-09-11 13:43:36 +0900

    Create 0297 Serialize and Deserialize Binary Tree.py

---
+++ 0297 Serialize and Deserialize Binary Tree.py
@@ -0,0 +1,38 @@
+class Codec:
+    def serialize(self, root: TreeNode) -> str:
+        queue = collections.deque([root])
+        result = ['#']
+        while queue:
+            node = queue.popleft()
+            if node:
+                queue.append(node.left)
+                queue.append(node.right)
+
+                result.append(str(node.val))
+            else:
+                result.append('#') 
+        return ' '.join(result)
+
+    def deserialize(self, data: str) -> TreeNode:
+        if data == '# #':
+            return None
+
+        nodes = data.split()
+
+        root = TreeNode(int(nodes[1]))
+        queue = collections.deque([root])
+        index = 2
+
+        while queue:
+            node = queue.popleft()
+            if nodes[index] is not '#':
+                node.left = TreeNode(int(nodes[index]))
+                queue.append(node.left)
+            index += 1
+
+            if nodes[index] is not '#':
+                node.right = TreeNode(int(nodes[index]))
+                queue.append(node.right)
+            index += 1
+
+        return root
