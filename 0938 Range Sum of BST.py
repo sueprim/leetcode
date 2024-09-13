197779ab6b3c51b477d45519b1f88e84adeba677
sueprim <164147163+sueprim@users.noreply.github.com>
2024-09-13 18:56:54 +0900

    Create 0938 Range Sum of BST.py

---
+++ 0938 Range Sum of BST.py
@@ -0,0 +1,12 @@
+class Solution:
+    def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
+        def dfs(node):
+            if not node:
+                return 0
+            if node.val < low:
+                return dfs(node.right)
+            elif node.val > high:
+                return dfs(node.left)
+            return node.val + dfs(node.left) + dfs(node.right)
+
+        return dfs(root)
